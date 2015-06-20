44592643097e5d96811f947dce732599519d79a0
Assaf <Assaf.Yossifoff@kcl.ac.uk>
2015-06-20 23:14:40 +0100

    pebble camera integrated

--- app/src/main/java/com/angelhack/nellie/MainActivity.java
+++ app/src/main/java/com/angelhack/nellie/MainActivity.java
@@ -12,24 +12,37 @@ import android.view.Menu;
 import android.view.MenuItem;
 import android.view.SurfaceView;
 
-import java.io.File;
-import java.io.IOException;
-import java.text.SimpleDateFormat;
-import java.util.Date;
+import android.content.Context;
+import android.widget.TextView;
 
+import com.getpebble.android.kit.PebbleKit;
+import com.getpebble.android.kit.util.PebbleDictionary;
+
+import java.util.UUID;
 
 public class MainActivity extends Activity {
 
-    static final int REQUEST_TAKE_PHOTO = 1;
-    String mCurrentPhotoPath;
-    Camera mCamera;
-    //InvisiblePreview mPreview;
+    private PebbleKit.PebbleDataReceiver mReceiver;
+    private TextView buttonView;
+    private UUID appUUID = UUID.fromString("036b24a1-7fa5-4acc-aef1-76296ab4d984");
+
+    private static final int
+            KEY_BUTTON_EVENT = 0,
+            BUTTON_EVENT_UP = 1,
+            BUTTON_EVENT_DOWN = 2,
+            BUTTON_EVENT_SELECT = 3,
+            KEY_VIBRATION = 4;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        startActivity(new Intent(this, PictureAction.class));
+
+        boolean connected = PebbleKit.isWatchConnected(getApplicationContext());
+        Log.i(getLocalClassName(), "Pebble is " + (connected ? "connected" : "not connected"));
+
+        PebbleKit.startAppOnPebble(getApplicationContext(), appUUID);
+
 
     }
 
@@ -50,6 +63,53 @@ public class MainActivity extends Activity {
             return true;
         }
         return super.onOptionsItemSelected(item);
+    }
+
+    @Override
+    protected void onResume() {
+        super.onResume();
+
+        mReceiver = new PebbleKit.PebbleDataReceiver(appUUID) {
+
+            @Override
+            public void receiveData(Context context, int transactionId, PebbleDictionary data) {
+                //ACK the message
+                PebbleKit.sendAckToPebble(context, transactionId);
+
+                Log.i(getLocalClassName(), "Pebble is communicating");
+
+
+                //Check the key exists
+                if (data.getUnsignedIntegerAsLong(KEY_BUTTON_EVENT) != null) {
+                    int button = data.getUnsignedIntegerAsLong(KEY_BUTTON_EVENT).intValue();
+
+                    switch (button) {
+                        case BUTTON_EVENT_UP:
+                            startActivity(new Intent(getApplicationContext(), PictureAction.class));
+                            break;
+                        case BUTTON_EVENT_DOWN:
+                            break;
+                        case BUTTON_EVENT_SELECT:
+                            break;
+                    }
+
+                    PebbleDictionary dict = new PebbleDictionary();
+                    dict.addString(5, "Samantha");
+                    PebbleKit.sendDataToPebble(context, appUUID, dict);
+
+                }
+            }
+
+        };
+
+        PebbleKit.registerReceivedDataHandler(this, mReceiver);
+    }
+
+    @Override
+    protected void onPause() {
+        super.onPause();
+
+        unregisterReceiver(mReceiver);
     }
 
 
