c16590e812a8656d1293f3f832af9e0df389ac50
Assaf <Assaf.Yossifoff@kcl.ac.uk>
2015-06-21 01:11:19 +0100

    pebble button takes picture

--- app/src/main/java/com/angelhack/nellie/MainActivity.java
+++ app/src/main/java/com/angelhack/nellie/MainActivity.java
@@ -6,24 +6,39 @@ import android.hardware.Camera;
 import android.net.Uri;
 import android.os.Bundle;
 import android.os.Environment;
-import android.provider.MediaStore;
 import android.util.Log;
 import android.view.Menu;
 import android.view.MenuItem;
-import android.view.SurfaceView;
 
 import android.content.Context;
+import android.view.SurfaceHolder;
+import android.view.SurfaceView;
 import android.widget.TextView;
+import android.widget.Toast;
 
 import com.getpebble.android.kit.PebbleKit;
 import com.getpebble.android.kit.util.PebbleDictionary;
 
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.text.SimpleDateFormat;
+import java.util.Date;
 import java.util.UUID;
 
-public class MainActivity extends Activity {
+public class MainActivity extends Activity implements SurfaceHolder.Callback {
+
+    /** New **/
+    Camera camera;
+    SurfaceView surfaceView;
+    SurfaceHolder surfaceHolder;
+
+    Camera.PictureCallback jpegCallback;
+    File mCurrentPhotoPath;
+    /** End New **/
 
     private PebbleKit.PebbleDataReceiver mReceiver;
-    private TextView buttonView;
     private UUID appUUID = UUID.fromString("036b24a1-7fa5-4acc-aef1-76296ab4d984");
 
     private static final int
@@ -43,8 +58,139 @@ public class MainActivity extends Activity {
 
         PebbleKit.startAppOnPebble(getApplicationContext(), appUUID);
 
+        /** New **/
+        surfaceView = (SurfaceView) findViewById(R.id.surfaceView);
+        surfaceHolder = surfaceView.getHolder();
+
+        // Install a SurfaceHolder.Callback so we get notified when the
+        // underlying surface is created and destroyed.
+        surfaceHolder.addCallback(this);
+
+        // deprecated setting, but required on Android versions prior to 3.0
+        surfaceHolder.setType(SurfaceHolder.SURFACE_TYPE_PUSH_BUFFERS);
+
+        jpegCallback = new Camera.PictureCallback() {
+            public void onPictureTaken(byte[] data, Camera camera) {
+                File pictureFile = getOutputMediaFile();
+                if (pictureFile == null){
+                    Log.d("CAMERA", "Error creating media file, check storage permissions.");
+                    return;
+                }
+
+                try {
+                    FileOutputStream fos = new FileOutputStream(pictureFile);
+                    fos.write(data);
+                    fos.close();
+                    mCurrentPhotoPath = pictureFile;
+                } catch (FileNotFoundException e) {
+                    Log.d("CAMERA", "File not found: " + e.getMessage());
+                } catch (IOException e) {
+                    Log.d("CAMERA", "Error accessing file: " + e.getMessage());
+                }
+
+                refreshCamera();
+            }
+        };
+        /** End New **/
 
     }
+
+    /** New **/
+    public void captureImage() {
+        camera.takePicture(null, null, jpegCallback);
+    }
+
+    public void refreshCamera() {
+        if (surfaceHolder.getSurface() == null) {
+            // preview surface does not exist
+            return;
+        }
+
+        // stop preview before making changes
+        try {
+            camera.stopPreview();
+        } catch (Exception e) {
+            // ignore: tried to stop a non-existent preview
+        }
+
+        // set preview size and make any resize, rotate or
+        // reformatting changes here
+        // start preview with new settings
+        try {
+            camera.setPreviewDisplay(surfaceHolder);
+            camera.startPreview();
+        } catch (Exception e) {
+
+        }
+    }
+
+    public void surfaceChanged(SurfaceHolder holder, int format, int w, int h) {
+        // Now that the size is known, set up the camera parameters and begin
+        // the preview.
+        refreshCamera();
+    }
+
+    public void surfaceCreated(SurfaceHolder holder) {
+        try {
+            // open the camera
+            camera = Camera.open();
+        } catch (RuntimeException e) {
+            // check for exceptions
+            System.err.println(e);
+            return;
+        }
+        Camera.Parameters param;
+        param = camera.getParameters();
+
+        // modify parameter
+        param.setPreviewSize(352, 288);
+        camera.setParameters(param);
+        try {
+            // The Surface has been created, now tell the camera where to draw
+            // the preview.
+            camera.setPreviewDisplay(surfaceHolder);
+            camera.startPreview();
+        } catch (Exception e) {
+            // check for exceptions
+            System.err.println(e);
+            return;
+        }
+    }
+
+    public void surfaceDestroyed(SurfaceHolder holder) {
+        // stop preview and release camera
+        camera.stopPreview();
+        camera.release();
+        camera = null;
+    }
+
+    /** Create a File for saving an image or video */
+    private static File getOutputMediaFile(){
+        // To be safe, you should check that the SDCard is mounted
+        // using Environment.getExternalStorageState() before doing this.
+
+        File mediaStorageDir = new File(Environment.getExternalStoragePublicDirectory(
+                Environment.DIRECTORY_PICTURES), "MyCameraApp");
+        // This location works best if you want the created images to be shared
+        // between applications and persist after your app has been uninstalled.
+
+        // Create the storage directory if it does not exist
+        if (! mediaStorageDir.exists()){
+            if (! mediaStorageDir.mkdirs()){
+                Log.d("MyCameraApp", "failed to create directory");
+                return null;
+            }
+        }
+
+        // Create a media file name
+        String timeStamp = new SimpleDateFormat("yyyyMMdd_HHmmss").format(new Date());
+        File mediaFile;
+        mediaFile = new File(mediaStorageDir.getPath() + File.separator +
+                "IMG_"+ timeStamp + ".jpg");
+
+        return mediaFile;
+    }
+    /** End New **/
 
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
@@ -85,7 +231,8 @@ public class MainActivity extends Activity {
 
                     switch (button) {
                         case BUTTON_EVENT_UP:
-                            startActivity(new Intent(getApplicationContext(), PictureAction.class));
+                            //startActivity(new Intent(getApplicationContext(), PictureAction.class));
+                            captureImage();
                             break;
                         case BUTTON_EVENT_DOWN:
                             break;
